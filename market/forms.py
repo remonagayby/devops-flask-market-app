d0f5b97d0fedda6bad7e21408f31caaa036e2646
remonagayby <remon@Ubuntu.myguest.virtualbox.org>
2024-05-25 19:39:46 +0300

    create validators for registeration page

--- market/forms.py
+++ market/forms.py
@@ -1,8 +1,23 @@
-from wtforms import Form, StringField, PasswordField, SubmitField
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import Length, EqualTo, Email, DataRequired, ValidationError
+from market.models import User
 
-class RegisterForm(Form):
-    username = StringField(label='User Name')
-    email_address = StringField(label='Email')
-    password1 = PasswordField(label='Password')
-    password2 = PasswordField(label='Confirm Password')
+class RegisterForm(FlaskForm):
+
+    # validate if the user_name & email_address is not exist in DB
+    def validate_user_name(self, username_to_check):
+        user = User.query.filter_by(username=username_to_check.data).first()
+        if user:
+            raise ValidationError('User Name Already Exists!')
+        
+    def validate_email_address(self, email_address_to_check):
+        email_address = User.query.filter_by(email_address=email_address_to_check.data).first()
+        if email_address:
+            raise ValidationError('Email Address Already Exist!')
+        
+    username = StringField(label='User Name:', validators=[Length(min=2, max=30), DataRequired()])
+    email_address = StringField(label='Email:', validators=[Email(), DataRequired()])
+    password1 = PasswordField(label='Password:', validators=[Length(min=6), DataRequired()])
+    password2 = PasswordField(label='Confirm Password:', validators=[EqualTo('password1'), DataRequired()])
     submit = SubmitField(label='Create Account')
--- market/routes.py
+++ market/routes.py
@@ -1,6 +1,7 @@
 from market import app
-from flask import render_template
-from market.models import Item
+from flask import render_template, redirect, url_for, flash, get_flashed_messages
+from market.models import Item, User
+from market import db
 from market.forms import RegisterForm
 
 
@@ -14,7 +15,17 @@ def market_page():
         items = Item.query.all()
         return render_template('market.html', items=items)
 
-@app.route("/register")
+@app.route("/register", methods=['GET', 'POST'])
 def register_page():
     form = RegisterForm()
+    if form.validate_on_submit():
+         user_to_create = User(user_name = form.username.data,
+                            email_address = form.email_address.data,
+                            password = form.password1.data)
+         db.session.add(user_to_create)
+         db.session.commit()
+         return redirect(url_for('market_page'))
+    if form.errors != {}:  # if there is no errors from the validations
+         for err_msg in form.errors.values():
+              flash(f'There is an error creating a user: {err_msg}', category='danger')
     return render_template('register.html', form=form)
